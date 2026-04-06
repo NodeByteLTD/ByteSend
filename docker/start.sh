d419840e3cce8906f90c01a1d3b16aac7ddd2acf
TheRealToxicDev <toxic.dev09@gmail.com>
2026-04-06 01:10:39 -0600

    more stuff again

--- docker/start.sh
+++ docker/start.sh
@@ -4,9 +4,9 @@ set -x
 
 echo "Deploying prisma migrations"
 
-pnpx prisma@6.6.0  migrate deploy --schema ./apps/web/prisma/schema.prisma
+pnpx prisma@6.6.0 migrate deploy --schema ./apps/web/prisma/schema.prisma
 
 echo "Starting web server"
 
-node apps/web/server.js
+pnpm -F web start
 
